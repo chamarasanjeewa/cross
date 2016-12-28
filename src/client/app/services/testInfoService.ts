928b36e084a960168e8be3599e0840210c8d259f
chamara <chamara.sanjeewa@gmail.com>
2016-12-28 16:31:28 +0530

    changes

---
+++ src/client/app/services/testInfoService.ts
@@ -0,0 +1,12 @@
+import { Http,Response,Headers,RequestOptions,RequestMethod,Request  } from '@angular/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+
+@Injectable()
+export class TestInfoService{
+    
+    constructor(){
+        
+    }
+    
+}
