826a7f0b5411df1200e0f5a944c933ceb833da12
chamarasanjeewa <chamara.sanjeewa@gmail.com>
2017-01-01 22:11:26 +0530

    added changes

--- src/client/app/dashBoard/dashBoard.component.spec.ts
+++ src/client/app/dashBoard/dashBoard.component.spec.ts
@@ -1,57 +1,98 @@
-import { Component } from '@angular/core';
-import { async,TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import {TestService} from '../services/user.service';
-import {HomeComponent} from '../home/home.component';
-import { Routes } from '@angular/router';
-import {DashBoardComponent} from './dashBoard.component';
-import { TestInfoService } from '../services/testInfoService';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashBoardRoutes } from './index';
-import {FirewallComponent} from '../fireWall/firewall.component'
-import {ItemInfoComponent} from '../itemInfo/itemInfo.component'
-import {BuildComponent} from '../build/index'
-import {MetricsInfoComponent} from '../metricsInfo/metricsInfo.component';
-import {BuildInfoComponent} from '../buildInfo/buildInfo.component';
-import {UnitTestInfoComponent} from '../unitTestInfo/unitTestInfo.component';
-import {FunctionalTestInfoComponent} from '../functionalTestInfo/functionalTestInfo.component';
-//import {Ng2GoogleChartModule} from 'ng2-googlechart';
-let helloService:any;
+//  import { Component } from '@angular/core';
+//  import { async,TestBed } from '@angular/core/testing';
+//  import {TwainComponent} from './testComponent';
+// import { FormsModule } from '@angular/forms';
+// import { RouterModule } from '@angular/router';
+// import {TestService} from '../services/user.service';
+// import {HomeComponent} from '../home/home.component';
+// import { Routes } from '@angular/router';
+// import {DashBoardComponent} from './dashBoard.component';
+ //import { TestInfoService } from '../services/testInfoService';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { DashBoardRoutes } from './index';
+// import {FirewallComponent} from '../fireWall/firewall.component'
+// import {ItemInfoComponent} from '../itemInfo/itemInfo.component'
+// import {BuildComponent} from '../build/index'
+// import {MetricsInfoComponent} from '../metricsInfo/metricsInfo.component';
+// import {BuildInfoComponent} from '../buildInfo/buildInfo.component';
+// import {UnitTestInfoComponent} from '../unitTestInfo/unitTestInfo.component';
+// import {FunctionalTestInfoComponent} from '../functionalTestInfo/functionalTestInfo.component';
+// import {ResultCompleteComponent} from '../Result/ResultComplete/resultComplete.component';
+// import {ResultAcceptedComponent} from '../Result/ResultAccepted/resultAccepted.component';
+// import {ResultRejectedComponent} from '../Result/ResultRejected/resultRejected.component';
 
-beforeEach(() => {
-  TestBed.configureTestingModule({
-      declarations: [DashBoardComponent,FirewallComponent,ItemInfoComponent,BuildComponent,UnitTestInfoComponent,MetricsInfoComponent,FunctionalTestInfoComponent,BuildInfoComponent],
-    imports: [
-      // HttpModule, etc.
-    ],
-    providers: [
+// let helloService:any;
+
+// beforeEach(() => {
+//   TestBed.configureTestingModule({
+//       declarations: [DashBoardComponent,FirewallComponent,ItemInfoComponent,BuildComponent,UnitTestInfoComponent,MetricsInfoComponent,FunctionalTestInfoComponent,BuildInfoComponent,ResultCompleteComponent,ResultAcceptedComponent,ResultRejectedComponent],
+//     imports: [
+//       // HttpModule, etc.
+//     ],
+//     providers: [
       
-      {provide: TestInfoService, useValue: helloService}
-    ]
+//       {provide: TestInfoService, useValue: helloService}
+//     ]
+//   });
+// });
+
+// it("can initialize", async(() => {
+//     TestBed.compileComponents().then(() => {
+//         const fixture = TestBed.createComponent(DashBoardComponent);
+//         let element = fixture.nativeElement;
+//         let component = fixture.componentInstance;
+
+//         fixture.detectChanges();
+//         expect(component).toBeDefined();
+//     });
+// }));
+
+import { ComponentFixture, TestBed,async  } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+import { BannerComponent } from './testComponent';
+
+
+describe('BannerComponent (templateUrl)', () => {
+
+  let comp:    BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
+
+  // async beforeEach
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BannerComponent], // declare the test component
+    })
+    .compileComponents();  // compile template and css
+  }));
+  
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BannerComponent);
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
   });
+
+  it('no title in the DOM until manually call `detectChanges`', () => {
+    expect(el.textContent).toEqual('');
+  });
+
+  it('should display original title', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain(comp.title);
+  });
+
+  it('should display a different test title', () => {
+    comp.title = 'Test Title';
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Test Title');
+  });
+
+  
 });
-
-it("can initialize", async(() => {
-    TestBed.compileComponents().then(() => {
-        const fixture = TestBed.createComponent(DashBoardComponent);
-        let element = fixture.nativeElement;
-        let component = fixture.componentInstance;
-
-        fixture.detectChanges();
-
-        expect(element).not.toBeNull();
-        expect(component).not.toBeNull();
-
-        //expect(helloService.greet).toHaveBeenCalled();
-
-        let header = element.querySelector("h1");
-        expect(header).not.toBeNull();
-        expect(header.textContent).toBe("Greeting test");
-
-        let greeting = element.querySelector("#greeting");
-        expect(greeting).not.toBeNull();
-        expect(greeting.textContent).toBe("greet");
-    });
-}));
